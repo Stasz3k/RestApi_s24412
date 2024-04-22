147f34bfb7c9ab245195f197a7d1dd4cdd292550
Stasz3k <s24412@pjwstk.edu.pl>
2024-04-23 00:08:13 +0200

    Part 2

--- Program.cs
+++ Program.cs
@@ -1,8 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using RestApi_s24412;
+
 var builder = WebApplication.CreateBuilder(args);
 
+
 builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<YourDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MyConnectionString")));
+
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+}
 
 var app = builder.Build();
 
@@ -13,9 +24,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
